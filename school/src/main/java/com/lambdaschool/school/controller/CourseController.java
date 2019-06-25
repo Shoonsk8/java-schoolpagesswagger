c942f5ad63e934bc0b01fb8570286bb461fe189e
Shoonsk8 <shoon@att.net>
2019-06-25 12:05:03 -0400

    initial

--- school/src/main/java/com/lambdaschool/school/controller/CourseController.java
+++ school/src/main/java/com/lambdaschool/school/controller/CourseController.java
@@ -37,3 +37,19 @@ public class CourseController
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
+//Instructions
+//
+//        Start with the school project.
+//
+//        Make the endpoint for finding all students pageable.
+//
+//        Make the endpoint for finding all students who have a name containing the given substring pageable.
+//
+//        Add custom Swagger documentation to all of the endpoints in StudentController. Make sure that Swagger doucment for the other the endpoints in other controller default to something useful.
+//
+//        Add custom Swagger documentation to each of the models - course, student, instructor.
+//
+//        Stretch Goals
+//
+//        Add the ability to assign existing students to existing courses. Use a custom query for this.
+//        Add Swagger Documentation to the endpoint that adds students to courses.
